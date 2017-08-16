7ebcae4d69ef407c349c6c71a6c4106fffa12e26
Arslangit config --global user.email Cheema <arslanch33ma@gmail.com>
2017-08-16 21:57:15 +0900

    Design Collection View

---
+++ coder-swag-shopping/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag-shopping
+//
+//  Created by Cheema Arslan Arshad on 2017/08/16.
+//  Copyright © 2017 Cheema Arslan Arshad. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
